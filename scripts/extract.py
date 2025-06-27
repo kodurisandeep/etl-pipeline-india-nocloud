feaf4aadbb26955157d046a9f52735bf3f6a97df
kodurisandeep <kodurisandeep@gmail.com>
2025-06-27 11:05:25 +0530

    Simulate update from dev

--- scripts/extract.py
+++ scripts/extract.py
@@ -6,6 +6,7 @@ def extract_data(csv_path: str) -> pd.DataFrame:
     try:
         df = pd.read_csv(csv_path)
         logging.info(f"Data extracted: {df.shape[0]} records")
+        ##test
         return df
     except FileNotFoundError:
         logging.warning("CSV file not found.")
